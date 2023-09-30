d7e64e73aaec928897d91a1769e75218b0036b60
arsscriptum <guillaumeplante.qc@gmail.com>
2023-09-30 04:26:34 -0400

    auto

--- Build.bat
+++ Build.bat
@@ -15,7 +15,7 @@ goto :init
 
 :init
     set "__scripts_root=%AutomationScriptsRoot%"
-    call :read_script_root development\build-automation  BuildAutomation
+    call :read_script_data development\build-automation  BuildAutomation
     set "__script_file=%~0"
     set "__target=%~1"
     set "__configuration=%~2"
@@ -35,16 +35,18 @@ goto :init
     goto :eof
 
 :header_err
-    echo.**************************************************
+    echo. ======================================================
     echo.This script is part of arsscriptum build wrappers.
-    echo.**************************************************
+    echo. ======================================================
     echo.
-    echo. YOU NEED TO HAVE THE BuildAutomation Scripts setup on you system...
+    echo. YOU NEED TO HAVE THE BuildAutomation Scripts setup
+    echo. on you system...
     echo. https://github.com/arsscriptum/BuildAutomation
     goto :eof
 
 
-:read_script_root
+:read_script_data
+    if not defined OrganizationHKCU::=          call :header_err && call :error_missing_path OrganizationHKCU::= & goto :eof
     set regpath=%OrganizationHKCU::=%
     for /f "tokens=2,*" %%A in ('REG.exe query %regpath%\%1 /v %2') do (
             set "__scripts_root=%%B"
@@ -124,3 +126,4 @@ goto :init
 
 :finished
     call %__lib_out% :__out_d_grn "Build complete"
+
